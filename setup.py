da052c212b194e89713ad2d8a71daf7de9c2b2f4
Bastien Gandouet <bastien@ouihelp.fr>
2018-07-20 14:49:08 +0200

    Release yesaide 1.4.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as r_file:
 
 setup(
     name='yesaide',
-    version='1.4.2dev',
+    version='1.4.2',
     license='MIT',
     author='Ouihelp Tech',
     author_email='tech@ouihelp.fr',
